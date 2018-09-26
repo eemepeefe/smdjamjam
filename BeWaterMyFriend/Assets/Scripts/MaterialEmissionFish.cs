055548ee9f1e149ea5bc49cdec475739a0a71ee1
jetese <jetese2@gmail.com>
2018-09-26 22:23:41 +0200

    Añadido emisivo a los objetos

---
+++ BeWaterMyFriend/Assets/Scripts/MaterialEmissionFish.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialEmissionFish : MonoBehaviour {
+    Material mat;
+    public Texture emissive;
+    private GameObject mainCharacter;
+    public float leftLimit;
+    public float rightLimit;
+    private string name;
+    // Use this for initialization
+    void Start()
+    {
+        name = gameObject.transform.parent.gameObject.name.Replace("(Clone)", "");
+        
+        emissive = Resources.Load<Texture>(name);
+        mat = GetComponent<Renderer>().material;
+        mat.EnableKeyword("_EMISSION");
+        mat.SetTexture("_EmissionMap", emissive);
+        mat.SetColor("_EmissionColor", Color.white);
+        mainCharacter = GameObject.Find("spot_light");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
