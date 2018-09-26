341d86dd29c85e73e884e5c250dfcbb9cd75be14
jetese <jetese2@gmail.com>
2018-09-26 16:28:11 +0200

    Generación automatica

--- BeWaterMyFriend/Assets/Scripts/GameController.cs
+++ BeWaterMyFriend/Assets/Scripts/GameController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
     public GameObject fish;
+    public GameObject cameraGame;
+    public GameObject[] pItems;
+    public GameObject[] nItems;
     FishMovement fishScript;
+    public int dificulty;
+    public int bonus;
+    private Vector3 generatedPosition;
+    public float yPosition;
+    public Vector3 leftSide;
+    public float cameraOfsetZ;
+    public float cameraOfsetPosition;
+    public float sideOfset;
+    public float timeToGenerrate;
+
+    private bool[,] positions;
     // Use this for initialization
-    void Start () {
+    void Start() {
         fishScript = fish.GetComponent<FishMovement>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyUp(KeyCode.A) )
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.A))
         {
             fishScript.SetFishLeftPosition();
         }
@@ -20,6 +36,41 @@ public class GameController : MonoBehaviour {
         {
             fishScript.SetFishRightPosition();
         }
-        
+
+    }
+
+    Vector3 GenerateRandomPosition()
+    {
+        int x;
+        int z;
+        do
+        {
+            x = Random.Range(0, 9);
+            z = Random.Range(0, 9);
+        } while (positions[x, z] != true);
+
+        positions[x, z] = true;
+        return new Vector3(x, yPosition, z);
+
+
+    }
+
+    IEnumerator GenerateElements()
+    {
+        yield return new WaitForSeconds(timeToGenerrate);
+        positions = new bool[9, 9];
+        for (int i = 0; i < dificulty; i++)
+        {
+            generatedPosition = GenerateRandomPosition();
+            generatedPosition = new Vector3(leftSide.x + generatedPosition.x * sideOfset, yPosition, cameraGame.transform.position.z + cameraOfsetPosition + generatedPosition.z * cameraOfsetZ);
+            Instantiate(nItems[Random.Range(0, nItems.Length - 1)], generatedPosition, Random.rotation);
+        }
+        for (int i = 0; i < bonus; i++)
+        {
+            generatedPosition = GenerateRandomPosition();
+            generatedPosition = new Vector3(leftSide.x + generatedPosition.x * sideOfset, yPosition, cameraGame.transform.position.z + cameraOfsetPosition + generatedPosition.z * cameraOfsetZ);
+            Instantiate(pItems[Random.Range(0, pItems.Length - 1)], generatedPosition, Random.rotation);
+        }
     }
 }
+   
