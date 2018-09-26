826ff04e853fbd7cf21e0a8820aa4700be9eebaa
jetese <jetese2@gmail.com>
2018-09-26 13:31:26 +0200

    Colisiones

---
+++ BeWaterMyFriend/Assets/Scripts/Collision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "AddLight")
+        {
+            Debug.Log("AddLight");
+        }
+        if (other.tag == "LessLight")
+        {
+            Debug.Log("LessLight");
+        }
+        if (other.tag == "AddVel")
+        {
+            Debug.Log("AddVel");
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- BeWaterMyFriend/Assets/Scripts/GameController.cs
+++ BeWaterMyFriend/Assets/Scripts/GameController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-    
-	// Use this for initialization
-	void Start () {
-		
+    public GameObject fish;
+    FishMovement fishScript;
+    // Use this for initialization
+    void Start () {
+        fishScript = fish.GetComponent<FishMovement>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyUp(KeyCode.A) )
+        {
+            fishScript.SetFishLeftPosition();
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            fishScript.SetFishRightPosition();
+        }
+        
+    }
 }
