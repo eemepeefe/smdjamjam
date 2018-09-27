614c5bb105e7e42ec1d29c9e25a124c55e619175
Eva <evishaemece@gmail.com>
2018-09-27 04:44:44 +0200

    Escena inicio

---
+++ BeWaterMyFriend/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeScene : MonoBehaviour {
+    public Sprite negro;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ChangeImage()
+    {
+        GameObject.Find("Background").GetComponent<Image>().sprite = negro;
+        StartCoroutine(ChangeMainScene());
+    }
+    IEnumerator ChangeMainScene()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(1);
+    }
+}
