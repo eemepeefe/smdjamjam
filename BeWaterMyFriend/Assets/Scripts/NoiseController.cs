b645a8c11c7fab849c320134ce836f26c597cc0d
Pepe Segade <pepest97@gmail.com>
2018-09-27 06:36:12 +0200

    Mar marino y sistemas de particulas

---
+++ BeWaterMyFriend/Assets/Scripts/NoiseController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseController : MonoBehaviour {
+
+    public Material mat;
+    float x = 1.0f;
+    float y = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        x = x + Random.Range(0, Time.deltaTime);
+        y = y + Random.Range(0, Time.deltaTime);
+        mat.SetFloat("_X", x);
+        mat.SetFloat("_Y", x);
+
+
+    }
+}
---
+++ BeWaterMyFriend/NoiseController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseController : MonoBehaviour {
+
+    public Material mat;
+
+	// Use this for initialization
+	void Start () {
+        m_Material = GetComponent<Renderer>().material;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+		
+	}
+}
